5dd7a6e5c9ed0a72aa444e66fa3c036ad9b580b0
Inclusit <camillefranzen@gmail.com>
2024-09-12 10:16:54 +0200

    Refactor robots.js and add Googlebot rules

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,19 @@
+import { userAgent } from "next/server";
+import { StoryblokCMS } from "../utils/cms";
+
 export default function Robots() {
-    return {
-        rules: {
-          userAgent: '*',
-          allow: '/*',
-          disallow: '/api/',
-        },
-        sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
-      }
+  return {
+    rules: [{
+      userAgent: "*",
+      allow: "/*",
+      disallow: "/api/",
+    },
+    {
+      userAgent: Googlebot,
+      allow: ['/'],
+      disallow: "/private/",
+    }
+    ],
+    sitemap: `${(process.env.NEXT_PUBLIC_SITE_URL)}/sitemap.xml`,
+  };
 }
